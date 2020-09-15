c5e8d33d0d5b8038f31982585d927fd7479bce98
Aldres <aldres98@gmail.com>
2020-09-16 01:28:11 +0300

    forms

--- src/app/request-form/request-form.component.ts
+++ src/app/request-form/request-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {NgForm} from '@angular/forms';
+import stations from '../../assets/charging_station_msk.json';
 
 @Component({
   selector: 'app-request-form',
@@ -9,13 +10,26 @@ import {NgForm} from '@angular/forms';
 })
 export class RequestFormComponent implements OnInit {
 
+  requestTypes = [
+    {id: 1, name: 'Изменить информацию'},
+    {id: 2, name: 'Изменить сокет'},
+    {id: 3, name: 'Удалить объект'}
+  ];
+
   stationId = null;
+  currentStation = null;
+  currentRequestType = 1;
   subscription: any;
 
   constructor( private actRoute: ActivatedRoute, private router: Router) { }
 
   onSubmit(form: NgForm): void{
-    console.log(form);
+    console.log(form.value);
+  }
+
+  assignRequestType(requestType): void {
+    this.currentRequestType = requestType;
+    console.log(requestType);
   }
 
   ngOnInit(): void {
@@ -24,6 +38,8 @@ export class RequestFormComponent implements OnInit {
         if (params.hasOwnProperty('id') !== '') {
           console.log(params.id);
           this.stationId = params.id;
+          this.currentStation = stations.features.find(el => el.properties['@id'] === this.stationId).properties;
+          console.log(this.currentStation);
         }
       });
 
